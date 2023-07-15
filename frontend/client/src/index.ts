a4474145255fe54aee6ebc307093d31724dec7a6
Polina <paulin-sh@yandex.ru>
2023-07-16 00:33:22 +0300

    add a raindrop

--- frontend/client/src/index.ts
+++ frontend/client/src/index.ts
@@ -1,4 +1,23 @@
 import {render, html} from 'lit';
-import './button.css';
 
-render(html`<div>Hello!</div>`, document.body);
+import './index.css';
+import './rain.css';
+
+render(
+  html`
+  <div class="outer">
+    <div class="background">
+      <div class="streak">
+        <div class="drop"></div>
+        <div class="splat"></div>
+      </div>
+    </div>
+    <div class="inner">
+      <p class="header">
+        Welcome home.
+      </p>
+    </div>
+  </div>
+  `,
+  document.body,
+);
